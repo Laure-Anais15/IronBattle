31d2cdf8cabe4a5c5a8ae2116e19a41ea4b98077
Laure-Anais15 <laure_anais@msn.com>
2025-05-08 21:30:37 +0200

    character class updated + creation wizard and warrior class files

---
+++ IronBattle/src/AttackerInterface.java
@@ -0,0 +1,4 @@
+public class AttackerInterface {
+
+    public void attack(character)
+}
--- IronBattle/src/CharacterClass.java
+++ IronBattle/src/CharacterClass.java
@@ -1,3 +1,23 @@
-public class CharacterClass {
+public abstract class Character {
+    private String id;
+    private String name;
+    private int hp; //add random hp limits depending on wizard/warrior
+    private boolean isAlive = true;
 
+    public Character (String name, int hp) {
+        this.name = name;
+        this.hp = hp;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName (String name) {
+        this.name = name;
+    }
+    public int getHp() {
+        return hp;
+    }
+    public void setHp (int hp) {
+        this.hp = hp;
+    }
 }
